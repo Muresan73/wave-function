99320a7907c02e16e72de176f9953e77d7ed9463
Muresan73 <grge73@outlook.com>
2023-11-05 16:09:52 +0100

    basic recursion

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,11 @@
-use wave_function::app::{App, AppResult};
-use wave_function::event::{Event, EventHandler};
-use wave_function::handler::handle_key_events;
-use wave_function::tui::Tui;
-use std::io;
-use tui::backend::CrosstermBackend;
-use tui::Terminal;
-
 mod node;
+use std::error;
 
+/// Application result type.
+pub type AppResult<T> = std::result::Result<T, Box<dyn error::Error>>;
 fn main() -> AppResult<()> {
-    // Create an application.
-    let mut app = App::new();
+  // Create an application.
 
-    // Initialize the terminal user interface.
-    let backend = CrosstermBackend::new(io::stderr());
-    let terminal = Terminal::new(backend)?;
-    let events = EventHandler::new(250);
-    let mut tui = Tui::new(terminal, events);
-    tui.init()?;
-
-    // Start the main loop.
-    while app.running {
-        // Render the user interface.
-        tui.draw(&mut app)?;
-        // Handle events.
-        match tui.events.next()? {
-            Event::Tick => app.tick(),
-            Event::Key(key_event) => handle_key_events(key_event, &mut app)?,
-            Event::Mouse(_) => {}
-            Event::Resize(_, _) => {}
-        }
-    }
-
-    // Exit the user interface.
-    tui.exit()?;
-    Ok(())
+  node::main();
+  Ok(())
 }
--- src/node.rs
+++ src/node.rs
@@ -1,4 +1,4 @@
-use std::usize;
+use std::{fmt::Display, usize};
 
 use ndarray::prelude::*;
 use rayon::prelude::*;
@@ -6,6 +6,14 @@ enum Feature {
   T1,
   T2,
   T3,
+}
+
+enum Direction {
+  Left,
+  Right,
+  Up,
+  Down,
+  Direct,
 }
 
 fn get_adjacent<const N: usize>(grid: &Array2<Node<N>>, x: usize, y: usize) -> [Option<&Node<N>>; 4] {
@@ -29,6 +37,27 @@ impl<const N: usize> Node<N> {
   }
 }
 
+impl Display for Node<3> {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(
+      f,
+      "{:?}",
+      match self.data {
+        [1, 0, 0] => "T1".to_string(),
+        [0, 1, 0] => "T2".to_string(),
+        [0, 0, 1] => "T3".to_string(),
+        [_, _, _] => format!("S{}", self.get_entropy()),
+      }
+    )
+  }
+}
+
+fn get3<const N: usize>(value: &Node<N>) -> Node<3> {
+  Node::<3> {
+    data: [value.data[0], value.data[1], value.data[2]],
+  }
+}
+
 impl<const N: usize> Default for Node<N> {
   fn default() -> Self {
     Self { data: [1; N] }
@@ -41,20 +70,45 @@ struct System<const N: usize> {
   rules: Array2<Feature>,
 }
 
+fn zip_mod<const N: usize>(origin: &mut Node<N>, trigger: &Features<N>) {
+  origin.data.iter_mut().zip(trigger.iter()).for_each(|(o, t)| {
+    *o *= *t;
+  });
+}
+
 impl<const N: usize> System<N> {
   // convoltion layer
-  fn propagate(&self, trigger: &Features<N>, x: usize, y: usize) {
-    let mut target = self.grid.get((x, y));
+  fn propagate(&mut self, trigger: &Features<N>, direction: Direction, x: usize, y: usize) {
+    let target = self.grid.get_mut((x, y));
     match target {
       Some(node) if node.get_entropy() > 1 => {
-        node.data.iter().zip(trigger.iter()).map(|(o, t)| {
-          let _ = *o * t;
-        });
+        zip_mod(node, trigger);
         if node.get_entropy() == 1 {
           let _ = rule_infer(&self.rules, trigger);
+          println!();
+          println!("{}", self.grid.map(get3));
+          self.propagate(trigger, Direction::Direct, x + 1, y);
+          self.propagate(trigger, Direction::Direct, x, y + 1);
+          self.propagate(trigger, Direction::Direct, x.saturating_sub(1), y);
+          self.propagate(trigger, Direction::Direct, x, y.saturating_sub(1));
         }
       }
       Some(_) | None => {}
+    }
+  }
+}
+
+fn rule_infer<const N: usize>(rules: &Array2<Feature>, trigger: &Features<N>) -> Features<N> {
+  [0; N]
+}
+
+impl From<Feature> for Features<3> {
+  fn from(value: Feature) -> Self {
+    use Feature::*;
+    match value {
+      T1 => [1, 0, 0],
+      T2 => [0, 1, 0],
+      T3 => [0, 0, 1],
     }
   }
 }
@@ -70,24 +124,10 @@ fn rule3(trigger: &Features<3>) -> Features<3> {
 }
 use Feature::*;
 
-fn rule_infer<const N: usize>(rules: &Array2<Feature>, trigger: &Features<N>) -> Features<N> {
-  [0; N]
-}
+pub fn main() {
+  use Direction::*;
 
-impl From<Feature> for Features<3> {
-  fn from(value: Feature) -> Self {
-    use Feature::*;
-    match value {
-      T1 => [1, 0, 0],
-      T2 => [0, 1, 0],
-      T3 => [0, 0, 1],
-      _ => todo!(),
-    }
-  }
-}
-
-fn main() {
-  let system = System {
+  let mut system = System {
     weights: [0.25; 3],
     grid: array![
       [Node::<3>::default(), Node::default(), Node::default()],
@@ -101,6 +141,17 @@ fn main() {
     [T1, T1, T1, T3],
     ],
   };
+
+  let trigger = [1, 0, 0];
+  let x = 1;
+  let y = 1;
+  system.grid.get((x, y));
+  println!("RND Start");
+  println!("{}", system.grid);
+
+  system.propagate(&trigger, Direct, 1, 1);
+  println!("RND 1");
+  println!("{}", system.grid);
 }
 
 #[cfg(test)]
@@ -109,7 +160,8 @@ mod tests {
 
   #[test]
   fn propagation() {
-    let system = System {
+    use Direction::*;
+    let mut system = System {
       weights: [0.25; 3],
       grid: array![
         [Node::<3>::default(), Node::default(), Node::default()],
@@ -124,6 +176,21 @@ mod tests {
       ],
     };
     let trigger = [1, 0, 0];
-    system.propagate(&trigger, 1, 1);
+    system.propagate(&trigger, Direct, 1, 1);
+  }
+
+  #[test]
+  fn iter_test() {
+    let mut original1 = Node::<3>::default();
+    let trigger1 = [1, 0, 0];
+    zip_mod1(&mut original1, &trigger1);
+
+    let mut original2 = Node::<3>::default();
+    let trigger2 = [1, 0, 0];
+    zip_mod2(&mut original2, &trigger2);
+
+    println!("a{:?}", original1.data);
+    println!("b{:?}", original2.data);
+    assert_eq!(original1.data, original2.data);
   }
 }
