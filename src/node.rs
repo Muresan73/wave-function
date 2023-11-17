9641b028fa745e0bb18a2ca0c15445df738cd615
Grge <grge73@outlook.com>
2023-11-17 23:52:59 +0100

    colorful propagation

--- src/node.rs
+++ src/node.rs
@@ -1,7 +1,8 @@
-use std::{fmt::Display, usize};
-
+use log::{debug, info};
 use ndarray::prelude::*;
 use rayon::prelude::*;
+use std::{fmt::Display, usize};
+use Feature::*;
 enum Feature {
   T1,
   T2,
@@ -27,6 +28,7 @@ fn get_adjacent<const N: usize>(grid: &Array2<Node<N>>, x: usize, y: usize) -> [
 
 type Features<const N: usize> = [usize; N];
 
+#[derive(Clone, PartialEq, Debug)]
 struct Node<const N: usize> {
   data: Features<N>,
 }
@@ -41,12 +43,17 @@ impl Display for Node<3> {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     write!(
       f,
-      "{:?}",
+      "{}",
       match self.data {
-        [1, 0, 0] => "T1".to_string(),
-        [0, 1, 0] => "T2".to_string(),
-        [0, 0, 1] => "T3".to_string(),
-        [_, _, _] => format!("S{}", self.get_entropy()),
+        [1, 0, 0] => "\x1b[30;41mT1\x1b[0m".to_string(),
+        [0, 1, 0] => "\x1b[30;42mT2\x1b[0m".to_string(),
+        [0, 0, 1] => "\x1b[30;43mT3\x1b[0m".to_string(),
+        [0, 0, 0] => "\x1b[0;31mXX\x1b[0m".to_string(),
+        [_, _, _] => match self.get_entropy() {
+          2 => "\x1b[0;34mS2\x1b[0m".to_string(),
+          3 => "\x1b[0;36mS3\x1b[0m".to_string(),
+          _ => format!("S{}", self.get_entropy()),
+        },
       }
     )
   }
@@ -76,30 +83,35 @@ fn zip_mod<const N: usize>(origin: &mut Node<N>, trigger: &Features<N>) {
   });
 }
 
-impl<const N: usize> System<N> {
+impl System<3> {
   // convoltion layer
-  fn propagate(&mut self, trigger: &Features<N>, direction: Direction, x: usize, y: usize) {
+  fn propagate(&mut self, trigger: &Features<3>, direction: Direction, x: usize, y: usize) {
+    info!("Intermidiate state:\n{}", &self.grid);
     let target = self.grid.get_mut((x, y));
-    match target {
-      Some(node) if node.get_entropy() > 1 => {
-        zip_mod(node, trigger);
+    if let Some(node) = target {
+      if node.get_entropy() > 1 {
+        debug!("trigger {:?}", trigger);
+        let rule = rule_infer(&direction, trigger);
+        debug!("rule: {:?}", rule);
+        debug!("node: {:?}", node);
+        zip_mod(node, &rule);
+        debug!("res: {:?}", node);
+        debug!("");
+
         if node.get_entropy() == 1 {
-          let _ = rule_infer(&self.rules, trigger);
-          println!();
-          println!("{}", self.grid.map(get3));
-          self.propagate(trigger, Direction::Direct, x + 1, y);
-          self.propagate(trigger, Direction::Direct, x, y + 1);
-          self.propagate(trigger, Direction::Direct, x.saturating_sub(1), y);
-          self.propagate(trigger, Direction::Direct, x, y.saturating_sub(1));
+          let data = node.data;
+          self.propagate(&data, Direction::Right, x + 1, y);
+          self.propagate(&data, Direction::Down, x, y + 1);
+          self.propagate(&data, Direction::Left, x.saturating_sub(1), y);
+          self.propagate(&data, Direction::Up, x, y.saturating_sub(1));
         }
       }
-      Some(_) | None => {}
     }
   }
 }
 
-fn rule_infer<const N: usize>(rules: &Array2<Feature>, trigger: &Features<N>) -> Features<N> {
-  [0; N]
+fn rule_infer(rules: &Direction, trigger: &Features<3>) -> Features<3> {
+  rule3(trigger)
 }
 
 impl From<Feature> for Features<3> {
@@ -115,43 +127,59 @@ impl From<Feature> for Features<3> {
 
 fn rule3(trigger: &Features<3>) -> Features<3> {
   match trigger {
-    [1, 0, 0] => [1, 0, 1],
-    [0, 1, 0] => [1, 1, 0],
-    [0, 0, 1] => [0, 0, 1],
-    [_, 0, _] => [1, 0, 1],
-    [_, _, _] => todo!(),
+    [1, 0, 0] => [0, 1, 1],
+    [0, 1, 0] => [1, 0, 1],
+    [0, 0, 1] => [1, 1, 0],
+    [_, _, _] => [1, 1, 1],
   }
 }
-use Feature::*;
+
+#[derive(Default)]
+struct Game {
+  round: u32,
+  system: System<3>,
+}
+
+impl Game {
+  fn new(system: System<3>) -> Self {
+    println!("Game Start");
+    println!("{}", system.grid);
+    Game { round: 0, system }
+  }
+  fn run(&mut self, trigger: &Features<3>, x: usize, y: usize) {
+    use Direction::*;
+    self.system.propagate(trigger, Direct, x, y);
+    self.round += 1;
+    println!("RND {}", self.round);
+    println!("{}", self.system.grid);
+  }
+}
+
+impl Default for System<3> {
+  fn default() -> Self {
+    Self {
+      weights: [0.25; 3],
+      grid: array![
+        [Node::default(), Node::default(), Node::default()],
+        [Node::default(), Node::default(), Node::default()],
+        [Node::default(), Node::default(), Node::default()],
+      ],
+      #[rustfmt::skip]
+        rules: array![
+        [T1, T1, T1, T2],
+        [T1, T1, T1, T3],
+        [T1, T1, T1, T3],
+        ],
+    }
+  }
+}
 
 pub fn main() {
-  use Direction::*;
-
-  let mut system = System {
-    weights: [0.25; 3],
-    grid: array![
-      [Node::<3>::default(), Node::default(), Node::default()],
-      [Node::default(), Node::default(), Node::default()],
-      [Node::default(), Node::default(), Node::default()],
-    ],
-    #[rustfmt::skip]
-    rules: array![
-    [T1, T1, T1, T2],
-    [T1, T1, T1, T3],
-    [T1, T1, T1, T3],
-    ],
-  };
-
-  let trigger = [1, 0, 0];
-  let x = 1;
-  let y = 1;
-  system.grid.get((x, y));
-  println!("RND Start");
-  println!("{}", system.grid);
-
-  system.propagate(&trigger, Direct, 1, 1);
-  println!("RND 1");
-  println!("{}", system.grid);
+  env_logger::init();
+  let mut game = Game::default();
+  game.run(&[1, 0, 0], 1, 1);
+  game.run(&[0, 1, 0], 1, 1);
+  game.run(&[1, 0, 0], 1, 2);
 }
 
 #[cfg(test)]
@@ -159,12 +187,29 @@ mod tests {
   use super::*;
 
   #[test]
+  fn same_game() {
+    use Direction::*;
+
+    let mut system = System::default();
+    let trigger = [1, 0, 0];
+    system.propagate(&trigger, Direct, 1, 1);
+    let trigger = [0, 1, 0];
+    system.propagate(&trigger, Direct, 1, 1);
+
+    let mut game = Game::default();
+    game.run(&[1, 0, 0], 1, 1);
+    game.run(&[0, 1, 0], 1, 1);
+
+    assert_eq!(system.grid, game.system.grid);
+  }
+
+  #[test]
   fn propagation() {
     use Direction::*;
     let mut system = System {
       weights: [0.25; 3],
       grid: array![
-        [Node::<3>::default(), Node::default(), Node::default()],
+        [Node::default(), Node::default(), Node::default()],
         [Node::default(), Node::default(), Node::default()],
         [Node::default(), Node::default(), Node::default()],
       ],
@@ -177,20 +222,5 @@ mod tests {
     };
     let trigger = [1, 0, 0];
     system.propagate(&trigger, Direct, 1, 1);
-  }
-
-  #[test]
-  fn iter_test() {
-    let mut original1 = Node::<3>::default();
-    let trigger1 = [1, 0, 0];
-    zip_mod1(&mut original1, &trigger1);
-
-    let mut original2 = Node::<3>::default();
-    let trigger2 = [1, 0, 0];
-    zip_mod2(&mut original2, &trigger2);
-
-    println!("a{:?}", original1.data);
-    println!("b{:?}", original2.data);
-    assert_eq!(original1.data, original2.data);
   }
 }
